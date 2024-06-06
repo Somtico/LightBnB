6f26b56ae4b5f1bd82fb29eb611b7a38800d8ad0
Somto Ufondu <somtoufondu@gmail.com>
2024-06-06 14:50:10 -0600

    Update the getAllProperties function to match the queries from the search form

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -19,21 +19,22 @@ const users = require("./json/users.json");
  */
 const getUserWithEmail = function (email) {
   const queryString = `
-    SELECT * FROM users
+    SELECT * 
+    FROM users
     WHERE email = $1;
   `;
 
   return pool
     .query(queryString, [email])
     .then((result) => {
-      if(result.rows.length > 0) {
+      if (result.rows.length > 0) {
         return result.rows[0];
       } else {
         return null;
       }
     })
     .catch((err) => {
-      console.log('getUserWithEmail query error:', err.message);
+      console.log("getUserWithEmail query error:", err.message);
       throw err;
     });
 };
@@ -45,21 +46,22 @@ const getUserWithEmail = function (email) {
  */
 const getUserWithId = function (id) {
   const queryString = `
-    SELECT * FROM users
+    SELECT * 
+    FROM users
     WHERE id = $1;
   `;
 
   return pool
     .query(queryString, [id])
     .then((result) => {
-      if(result.rows.length > 0) {
+      if (result.rows.length > 0) {
         return result.rows[0];
       } else {
         return null;
       }
     })
     .catch((err) => {
-      console.log('getUserWithId query error:', err.message);
+      console.log("getUserWithId query error:", err.message);
       throw err;
     });
 };
@@ -82,7 +84,7 @@ const addUser = function (user) {
       return result.rows[0];
     })
     .catch((err) => {
-      console.log('addUser query error:', err.message);
+      console.log("addUser query error:", err.message);
       throw err;
     });
 };
@@ -96,7 +98,8 @@ const addUser = function (user) {
  */
 const getAllReservations = function (guest_id, limit = 10) {
   const queryString = `
-    SELECT * FROM properties
+    SELECT * 
+    FROM properties
     JOIN reservations ON properties.id = property_id
     JOIN property_reviews ON properties.id = property_reviews.property_id
     WHERE reservations.guest_id = $1
@@ -109,7 +112,7 @@ const getAllReservations = function (guest_id, limit = 10) {
       return result.rows;
     })
     .catch((err) => {
-      console.log('getAllReservations query error:', err.message);
+      console.log("getAllReservations query error:", err.message);
       throw err;
     });
 };
@@ -123,18 +126,64 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = (options, limit = 10) => {
-  const queryString = `
-  SELECT * FROM properties
-  LIMIT $1;
+  const queryParams = [];
+
+  let queryString = `
+  SELECT properties.*, AVG(property_reviews.rating) AS average_rating 
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
 `;
 
+  const whereClause = [];
+
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    whereClause.push(`city LIKE $${queryParams.length}`);
+  }
+
+  if (options.owner_id) {
+    queryParams.push(options.owner_id);
+    whereClause.push(`owner_id = $${queryParams.length}`);
+  }
+
+  if(options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100);
+    whereClause.push(`cost_per_night >= $${queryParams.length}`)
+  }
+
+  if(options.maximum_price_per_night) {
+    queryParams.push(options.maximum_price_per_night * 100);
+    whereClause.push(`cost_per_night <= $${queryParams.length}`)
+  }
+
+  if(whereClause.length > 0) {
+    queryString += `  WHERE ${whereClause.join(' AND ')}`;
+  }
+
+  queryString += `
+  GROUP BY properties.id
+  `;
+
+  if(options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`
+  }
+
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+  console.log(queryString, queryParams);
+
   return pool
-    .query(queryString, [limit])
+    .query(queryString, queryParams)
     .then((result) => {
       return result.rows;
     })
     .catch((err) => {
-      console.log('getAllProperties query error:', err.message);
+      console.log("getAllProperties query error:", err.message);
       throw err;
     });
 };
